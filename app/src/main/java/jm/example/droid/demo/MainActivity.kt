b9a27eab43add33bf39fd4e2efda48c58cef113d
jiang.junmin <409850956@qq.com>
2022-11-06 00:46:55 +0800

    use spotless

--- app/src/main/java/jm/example/droid/demo/MainActivity.kt
+++ app/src/main/java/jm/example/droid/demo/MainActivity.kt
@@ -1,12 +1,36 @@
+/*
+ * Copyright 2022 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package jm.example.droid.demo
 
 import android.os.Bundle
-import com.google.android.material.bottomnavigation.BottomNavigationView
+import android.view.SurfaceView
+import android.view.View
+import android.widget.Button
+import android.widget.FrameLayout
+import android.widget.LinearLayout
+import android.widget.RelativeLayout
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
+import androidx.recyclerview.widget.RecyclerView
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import jm.example.droid.demo.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -16,6 +40,14 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
+        val r = View(this)
+        val g = FrameLayout(this)
+        val ll = LinearLayout(this)
+        val ga = RecyclerView(this)
+        val f = Button(this)
+        val tt = TextView(this)
+        val sf = SurfaceView(this)
+        val rr = RelativeLayout(this)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
